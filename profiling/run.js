0a59a7d51a399ed61ca07f5fe9cbf3a6bfdd0d96
Oleg Shepotkin <music.wave.form7@gmail.com>
2024-12-13 15:09:32 +0300

    (profiling): memory and performance

---
+++ profiling/run.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+function notOptimisedCode(){
+    for (let i = 0; i < 1e7; i++) {} // Intentional bottleneck
+}
+
+const requests = [];
+const server = http.createServer((req, res) => {
+  console.log('Hit and Run');
+  requests.push(req);
+  notOptimisedCode();
+  res.end('Hello, World!');
+});
+
+server.listen(3000, () => console.log('Server running on port 3000'));
